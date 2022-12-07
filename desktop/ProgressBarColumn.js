abb098571d908f8a284c4ef6c8462e53ede2c896
Server2GJ <GermanGomezJimenez@gmail.com>
2022-12-07 11:18:03 -0500

    Create propierties 3- Color font and color progress. Version: 1.0.0.0

--- desktop/ProgressBarColumn.js
+++ desktop/ProgressBarColumn.js
@@ -2,7 +2,11 @@ bizagi.rendering.basicUserField.extend("bizagi.rendering.ProgressBarColumn",{}, 
 
     /*************************************************************/
     /* methods to be overriden by the implementations            */
+    /* 2GJ: Widget para representar el porcentaje de avance      */
+    /* animado en un render de coleccion.                        */
+    /* 2GJ (German J. Gomez Jimenez).                            */
     /*************************************************************/
+    
     getEditableControl: function () {
         return this.getGenericControl();
     },
@@ -22,44 +26,61 @@ bizagi.rendering.basicUserField.extend("bizagi.rendering.ProgressBarColumn",{}, 
         self.myinput = $("<div>");
         self.myinput.addClass("ProgressBarColumn_MainDivClass");
         
-        // Crear div principal de control.
-        const eleCtrlProgBar = document.createElement("div"); 
+        //2GJ: Crear div principal de control.
+        const eleCtrlProgBar = document.createElement("div");
+        //2GJ: Clases del div principal.
         eleCtrlProgBar.classList.add("control-progressBar");
+        //2GJ: Estilos del div principal.
         eleCtrlProgBar.style.height = properties.heightControl || '20px';
         eleCtrlProgBar.style.width = properties.widthControl || '100%';
         
-        // Crear div de la barra completa de progreso.
+        //2GJ: Crear div de la barra completa de progreso.
         const eleProgressBar = document.createElement("div"); 
+        //2GJ: Clases y atributos del div barra completa de progreso.
         eleProgressBar.classList.add("barra_progreso");
         eleProgressBar.setAttribute('id', 'barra_progreso');
+        //2GJ: Estilos de la barra completa de progreso.
         eleProgressBar.style.borderRadius = properties.borderRadius || '20px';
         eleProgressBar.style.backgroundColor = properties.containerBackColor || '#5a2020';
-        eleProgressBar.style.boxShadow = properties.containerBoxShadow || '#c74d4d';
+        var contColorSha = properties.containerBoxShadow || '#c74d4d'
+        eleProgressBar.style.boxShadow = 'inset 6px 6px 12px ' + contColorSha + ', inset -6px -6px 12px ' + contColorSha;
         
-        
-     
-        // Crear div del progreso.
+        //2GJ: Crear div del progreso.
         const eleProgress = document.createElement("div");
+        //2GJ: Clases y atributos del div barra de progreso.
         eleProgress.classList.add("progreso");
         eleProgress.setAttribute('id', 'progreso');
+        //2GJ: Estilos del div barra completa de progreso.
+        eleProgress.style.color = properties.progressNumColor || '#f0eeee';
         eleProgress.style.borderRadius = properties.borderRadius || '20px';
+        eleProgress.style.backgroundColor = properties.progressBackColor || '#4dd841';
+        var progColorSha = properties.progressBoxShadow || '#4dd841'
+        eleProgress.style.boxShadow = '0 0 10px ' + progColorSha + ', 0 0 20px ' + progColorSha;
         
         
-        // Crear div valor del progreso en nunmeros.
-        const elePprogessVal = document.createElement("div");
-        elePprogessVal.setAttribute('id', 'porcentaje');
-        elePprogessVal.innerText = bindedXpathValue;
+        //2GJ: Crear div valor del progreso en nunmeros.
+        const eleProgessVal = document.createElement("div");
+        //2GJ: Clases y atributos div del valor del progreso en nunmeros.
+        //eleProgessVal.classList.add("porcentaje");
+        eleProgessVal.setAttribute('id', 'porcentajeValue');
+        //2GJ: Estilo del div del valor del progreso en nunmeros.
+        eleProgessVal.innerText = bindedXpathValue;
         
+        
+        
+        //2GJ: Se enlazan los objetos html.
         self.myinput.append(eleCtrlProgBar);
         eleCtrlProgBar.append(eleProgressBar);
         eleProgressBar.append(eleProgress);
-        eleProgress.append(elePprogessVal);
+        eleProgress.append(eleProgessVal);
         
 
-    
+        //2GJ: Define la velocidad de la animacion.
         let vel = properties.animationSpeed || 7;
         vel = (((vel - 10) * (-1)) * 20);
     
+    
+        //2GJ: Maneja la animacion del control.
         let cantidad = 0;
         let totalPorcentaje = eleProgress.innerText;
         
@@ -69,7 +90,7 @@ bizagi.rendering.basicUserField.extend("bizagi.rendering.ProgressBarColumn",{}, 
 
             if (cantidad == totalPorcentaje) {
                 clearInterval(tiempo);
-                eleProgressBar.style.backgroundColor = "#ac3f1700";
+                eleProgressBar.style.backgroundColor = properties.progressBackColor || '#4dd841';
             }
 
             eleProgress.textContent = cantidad + "%";
